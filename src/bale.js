3360ffae225e68f7a16631897247de5aca7b394c
Jahangir Zinedine <jzinedine@gmail.com>
2019-02-13 10:53:54 +0330

    added date and id to Message class

--- src/bale.js
+++ src/bale.js
@@ -2,29 +2,35 @@
 // import {Message} from "./message"
 exports.__esModule = true;
 var Message = /** @class */ (function () {
-    function Message(sender, time, content, likes) {
+    function Message(sender, time, content, likes, date, id) {
+        if (content === void 0) { content = 'فایل'; }
+        if (likes === void 0) { likes = '0'; }
         this.sender = sender;
         this.time = time;
         this.content = content;
         this.likes = likes;
+        this.date = date;
+        this.id = id;
     }
     return Message;
 }());
 exports.Message = Message;
 function collectMsgObjects() {
     var msgs = document.getElementsByClassName("message");
-    var msgObjs = [];
+    var msgObjs = [], j = 0;
     for (var _i = 0, _a = msgs; _i < _a.length; _i++) {
         var msg = _a[_i];
         var result = buildMsgObject(msg);
-        if (result != null)
+        if (result != null) {
+            result.id = j++;
             msgObjs.push(result);
+        }
     }
     return msgObjs;
 }
 function buildMsgObject(msg) {
     if (!msg.classList.contains("message--loading")) {
-        var sender = '', content = '', time = '', likes = '0';
+        var sender = void 0, content = void 0, time = void 0, likes = void 0;
         var senderEls = msg.getElementsByClassName('message__sender__name');
         if (senderEls.length > 0)
             sender = senderEls[0].innerText;
@@ -36,6 +42,24 @@ function buildMsgObject(msg) {
             content = contentEls[0].innerText;
         if (msg.getElementsByClassName('counter').length > 0)
             likes = msg.getElementsByClassName('counter')[0].innerText;
-        return new Message(sender, time, content, likes);
+        return new Message(sender, time, content, likes, findDate(msg));
     }
+}
+function findDate(msg) {
+    if (msg.previousElementSibling == null)
+        return '';
+    else if (msg.previousElementSibling.classList.contains('date-divider'))
+        return msg.previousElementSibling.innerText;
+    else {
+        return findDate(msg.previousElementSibling);
+    }
+}
+function prepare_elastic_ingest() {
+    var msgObjs = collectMsgObjects();
+    var elastic_input = '';
+    for (var _i = 0, msgObjs_1 = msgObjs; _i < msgObjs_1.length; _i++) {
+        var msgObj = msgObjs_1[_i];
+        elastic_input += "{ \"index\" : { \"_index\" : \"bale\", \"_type\" : \"_doc\", \"_id\" : \"" + msgObj.id + "\" } }\n" + JSON.stringify(msgObj) + "\n";
+    }
+    return elastic_input;
 }
--- src/bale.ts
+++ src/bale.ts
@@ -1,38 +1,60 @@
 // import {Message} from "./message"
 
-export class Message{
-    constructor(public sender: string, public time: string, public content: string, public likes: string){}
+export class Message {
+    constructor(public sender?: string, public time?: string, public content: string = 'فایل', public likes: string = '0', public date?: string, public id?: number) { }
 }
 
-function collectMsgObjects(): Message[]{
-    const msgs= document.getElementsByClassName("message");
-    let msgObjs: Message[]= [];
-    for(let msg of <any>msgs){
-        let result= buildMsgObject(msg as HTMLElement)
-        if(result != null)
+function collectMsgObjects(): Message[] {
+    const msgs = document.getElementsByClassName("message");
+    let msgObjs: Message[] = [], j = 0;
+    for (let msg of <any>msgs) {
+        let result = buildMsgObject(msg as HTMLElement)
+        if (result != null) {
+            result.id = j++;
             msgObjs.push(result);
+        }
     }
     return msgObjs;
 }
 
-function buildMsgObject(msg: HTMLElement){
-    if(!msg.classList.contains("message--loading")){
-        let sender= '', content='', time='', likes = '0';
-        
+function buildMsgObject(msg: HTMLElement) {
+    if (!msg.classList.contains("message--loading")) {
+        let sender, content, time, likes;
+
         const senderEls = msg.getElementsByClassName('message__sender__name');
-        if(senderEls.length>0)
-            sender= (senderEls[0] as HTMLElement).innerText;
-        
+        if (senderEls.length > 0)
+            sender = (senderEls[0] as HTMLElement).innerText;
+
         const timeEls = msg.getElementsByClassName('message__timestamp');
-        if(timeEls.length>0)
-            time= (timeEls[0] as HTMLElement).innerText;
-        
+        if (timeEls.length > 0)
+            time = (timeEls[0] as HTMLElement).innerText;
+
         const contentEls = msg.getElementsByClassName('message__content');
-        if(contentEls.length>0)
-            content= (contentEls[0] as HTMLElement).innerText;
-        
-        if(msg.getElementsByClassName('counter').length > 0)
-            likes= (msg.getElementsByClassName('counter')[0] as HTMLElement).innerText;
-        return new Message(sender, time, content, likes);    
+        if (contentEls.length > 0)
+            content = (contentEls[0] as HTMLElement).innerText;
+
+        if (msg.getElementsByClassName('counter').length > 0)
+            likes = (msg.getElementsByClassName('counter')[0] as HTMLElement).innerText;
+        return new Message(sender, time, content, likes, findDate(msg));
     }
+}
+
+function findDate(msg: HTMLElement): string {
+    if (msg.previousElementSibling == null)
+        return '';
+    else if (msg.previousElementSibling.classList.contains('date-divider'))
+        return (msg.previousElementSibling as HTMLElement).innerText;
+    else {
+        return findDate(msg.previousElementSibling as HTMLElement);
+    }
+}
+
+function prepare_elastic_ingest() {
+    let msgObjs = collectMsgObjects();
+    let elastic_input: string = '';
+    for (let msgObj of msgObjs) {
+        elastic_input += `{ "index" : { "_index" : "bale", "_type" : "_doc", "_id" : "${msgObj.id}" } }
+${JSON.stringify(msgObj)}\n`
+    }
+    return elastic_input;
 }
